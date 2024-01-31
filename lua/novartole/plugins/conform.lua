cdb7b14634743e4c31818dbd2150939e32c6978a
Artem Novikov <novartole@gmail.com>
2024-02-01 02:19:19 +0300

    feat[plugins, conform]: Removed rust from formatters

--- lua/novartole/plugins/conform.lua
+++ lua/novartole/plugins/conform.lua
@@ -16,8 +16,8 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
-			rust = { "rustfmt" },
 			toml = { "taplo" },
+			-- and keep built-it support of rustfmt
 		},
 		format_on_save = {
 			timeout_ms = 500,

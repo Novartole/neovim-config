3477fd4a4dc998ff7a77137b1e9525bb243fe03e
Artem Novikov <novartole@gmail.com>
2024-02-01 02:21:26 +0300

    feat[plugins, harpoon]: Added open/close bindings for Normal mode

--- lua/novartole/plugins/harpoon.lua
+++ lua/novartole/plugins/harpoon.lua
@@ -9,7 +9,16 @@ return {
 				require("harpoon.term").gotoTerminal(1)
 				cmd.startinsert()
 			end,
-			{ desc = "Open terminal using Harpoon in background" },
+			{ desc = "Open terminal using Harpoon in background (in Terminal mode)" },
+		},
+		{
+			"<leader>T",
+			function()
+				local cmd = vim.cmd
+				cmd.split()
+				require("harpoon.term").gotoTerminal(1)
+			end,
+			{ desc = "Open terminal using Harpoon in background (in Normal mode)" },
 		},
 		{
 			"<leader>hm",
@@ -23,9 +32,14 @@ return {
 		require("harpoon").setup(opts)
 
 		local cmd = vim.cmd
+
 		vim.keymap.set("t", "<C-x>", function()
 			cmd.stopinsert()
 			cmd.close()
-		end, { desc = "Close terminal" })
+		end, { desc = "Close terminal from Terminal mode" })
+
+		vim.keymap.set("n", "<C-x>", function()
+			cmd.close()
+		end, { desc = "Close terminal from Normal mode" })
 	end,
 }

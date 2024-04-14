eadff2cfb6a58ee2e079d780672997f80ad5f027
Artem Novikov <novartole@gmail.com>
2024-04-15 02:08:29 +0300

    feat[plugins, rustaceanvim]: Changed padding

--- lua/novartole/plugins/rustaceanvim.lua
+++ lua/novartole/plugins/rustaceanvim.lua
@@ -1,11 +1,10 @@
 return {
 	"mrcjkb/rustaceanvim",
-	version = "^3",
+	version = "^4",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"lvimuser/lsp-inlayhints.nvim",
 		"mfussenegger/nvim-dap",
-		"MunifTanjim/nui.nvim",
 	},
 	ft = { "rust" },
 	opts = {
@@ -30,6 +29,8 @@ return {
 				max_width = 120,
 
 				offset_x = 1, -- a trick to align floating window
+
+				padding_x = 2,
 			},
 		},
 		server = {
@@ -78,6 +79,16 @@ return {
 			-- force focusing in certain cases
 			--
 			if _opts and vim.tbl_contains({ "rustc-explain-error", "ra-render-diagnostic" }, _opts.focus_id) then
+				-- add horizontal padding, but skip action section
+				-- (such kind of events always have the section)
+				--
+				local padding = string.rep(" ", _opts.padding_x or 0)
+				if string.len(padding) > 0 then
+					for index = 3, #contents do
+						contents[index] = string.format("%s%s%s", padding, contents[index], padding)
+					end
+				end
+
 				local bufnr, winnr = open_floating_preview(contents, syntax, _opts)
 
 				vim.api.nvim_set_current_win(winnr)

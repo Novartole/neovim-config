4e62e6e55c962e9bf999748c977155a2b44d2ea5
Artem Novikov <novartole@gmail.com>
2024-07-30 19:53:01 +0300

    feat[plugins, inlayhints]: Tuned in hint appearance

--- lua/novartole/plugins/inlayhints.lua
+++ lua/novartole/plugins/inlayhints.lua
@@ -1,5 +1,14 @@
 return {
 	"lvimuser/lsp-inlayhints.nvim",
 	lazy = true,
-	config = true,
+	opts = {
+		inlay_hints = {
+			type_hints = {
+				prefix = "▸ ",
+				remove_colon_start = true,
+			},
+			labels_separator = " ",
+			highlight = "DapUIBreakpointsDisabledLine",
+		},
+	},
 }

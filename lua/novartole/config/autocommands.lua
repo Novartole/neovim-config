e5256b20eea13f84fea4489800d3e4133cb30118
Artem Novikov <novartole@gmail.com>
2024-07-30 19:38:43 +0300

    feat[config, autocommands]: Hightlighted yanked, jumped to last pos on openning

--- lua/novartole/config/autocommands.lua
+++ lua/novartole/config/autocommands.lua
@@ -2,9 +2,33 @@ local function augroup(name)
 	return vim.api.nvim_create_augroup(name, { clear = true })
 end
 
+-- respect current spliting form while resizing
+--
 vim.api.nvim_create_autocmd("VimResized", {
 	group = augroup("resize_splits"),
 	callback = function()
 		vim.cmd.wincmd("=")
+	end,
+})
+
+-- highlight yanked text
+--
+vim.api.nvim_create_autocmd("TextYankPost", {
+	pattern = "*",
+	command = "silent! lua vim.highlight.on_yank({ timeout = 200 })",
+})
+
+-- jump to last edit position on opening file
+--
+vim.api.nvim_create_autocmd("BufReadPost", {
+	pattern = "*",
+	callback = function()
+		if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+			-- except for in git commit messages
+			-- https://stackoverflow.com/questions/31449496/vim-ignore-specifc-file-in-autocommand
+			if not vim.fn.expand("%:p"):find(".git", 1, true) then
+				vim.cmd('exe "normal! g\'\\""')
+			end
+		end
 	end,
 })
